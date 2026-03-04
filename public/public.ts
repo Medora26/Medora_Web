49e1bdbd5a20bb5dfe95015358df452a4be64e27
IgYaHiko <subhrokolay2@gmail.com>
2026-03-05 03:11:12 +0530

    [PATCH]-[64]:enhance-the-homepage

--- public/public.ts
+++ public/public.ts
@@ -2,10 +2,13 @@ import DARK from "../public/dark.png"
 import LIGHT from "../public/light.png"
 import DARK_ONBOARDING from "../public/onboarding.png"
 import LIGHT_ONBOARDING from "../public/light-on.png"
-
+import DASHBOARD_DARK from './dash-dark.png'
+import DASHBOARD_LIGHT from "./dash-light.png"
 export const Public = {
    DARK,
    LIGHT,
    DARK_ONBOARDING,
-   LIGHT_ONBOARDING
+   LIGHT_ONBOARDING,
+   DASHBOARD_DARK,
+   DASHBOARD_LIGHT
 }
